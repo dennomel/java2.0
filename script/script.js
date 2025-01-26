6b574c02f3a20c3a9a74b83556be9f50816ef538
dennomel <drkdennomel.gmail.com>
2025-01-26 11:09:47 +0300

    added results tag on the script

--- script/script.js
+++ script/script.js
@@ -21,4 +21,37 @@ document.addEventListener,
       "Kofi",
       "Kwames",
     ];
+
+    const dayOfWeek = Math.floor(
+      century / 4 -
+        2 * century -
+        1 +
+        (5 * year) / 4 +
+        (26 * (month + 1)) / 10 +
+        day
+    );
+
+    return (dayOfWeek + 7) % 7;
   };
+
+  form.addEventListener("submit",(event)=>
+    {
+    event.preventDefault();
+
+    const dateOfBirthInput= document.getElementById("birthday").value;
+    const genderInput=document.getElementById("gender").value;
+  
+
+if (!dateOfBirth || !genderInput){
+    resultDiv.textContent="Pleae key in a valid birthday and select your gender";
+    resultDiv.style.color= "red";
+    return;
+}
+
+const dateOfBirth; new dateOfBirth(dateOfBirthInput);
+
+if (isNaN(dateOfBirth.getTime())) {
+    resultDiv.textContent= "Invalid date. Try again";
+    resultDiv.style,color="red";
+    return;
+}
