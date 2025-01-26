6e3c311d4ee2f556de827434866a7924be64926d
dennomel <drkdennomel.gmail.com>
2025-01-26 08:37:17 +0300

    edited the javascript

---
+++ script/script.js
@@ -0,0 +1,24 @@
+document.addEventListener,
+  () => {
+    const form = document.getElementById("akan-form");
+    const resultDiv = document.getElementById("result");
+
+    const femaleNames = [
+      "Akosua",
+      "Adwoa",
+      "Abenna",
+      "Akua",
+      "Yaa",
+      "Afua",
+      "Ama",
+    ];
+    const maleNames = [
+      "Kwasi",
+      "Kwadwo",
+      "Kwabena",
+      "kwaku",
+      "Yaw",
+      "Kofi",
+      "Kwames",
+    ];
+  };
